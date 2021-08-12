1edc44db6def88e3a19874007829f88d6f742977
Renato Aquino <repitriano12@gmail.com>
2021-08-12 00:56:35 -0300

    update: fixing the bug when caling the api from the gateway

---
+++ TeaAPI/Controllers/TeaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TeaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeaController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "tea from tomezera", "tea from gabriel", "tea from rachel", "tea from pedrin"
+        };
+
+        [HttpGet]
+        public IEnumerable<Tea> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Tea
+            {
+                Name = "TEA API",
+                TeaType = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
